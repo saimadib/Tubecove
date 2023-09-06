0fe22f566bd07b3dc83e46fc0f518aef4a38f38d
Saim <saimadib2002@gmail.com>
2023-09-06 23:36:50 +0530

    fixing auth

--- Backend/controllers/outhPermission.js
+++ Backend/controllers/outhPermission.js
@@ -39,7 +39,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.clientID,
   clientSecret: process.env.clientSecret,
-  callbackURL: 'http://localhost:5000/api/creator/auth/google/callback',
+  callbackURL: 'https://tubecove.zenservers.tech/api/creator/auth/google/callback',
   userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
 },
 async ( accessToken, refreshToken, profile, done) => {
