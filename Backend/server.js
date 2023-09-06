2a0072bef8dd73d61fb2df71ef4138f811506353
Saim <saimadib2002@gmail.com>
2023-09-06 22:06:55 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,7 @@ const cors=require("cors");
 
 app.use(express.json());
 // CORS configuration
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
+app.use(cors());
 mongoose.connect(`mongodb+srv://admin-saim:${process.env.MONGO_SECRET}@cluster0.fzd8b5a.mongodb.net/Youedit`);
 
 // Import and use your route handlers here
@@ -23,6 +18,14 @@ const editorRouter = require("./routes/editorRoutes");
 app.use("/api/creator", creatorRouter);
 app.use("/api/editor", editorRouter);
 
+
+app.use(express.static("public"));
+
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 
 app.listen(port, () => {
