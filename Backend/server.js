25172a25d64217ec6a4a8fce66efb7780d5a02b8
Saim <saimadib2002@gmail.com>
2023-09-06 22:21:18 +0530

    working on local

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors=require("cors");
+const path=require ("path");
 
 
 app.use(express.json());
